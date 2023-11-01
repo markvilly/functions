1cca2d067942369fdb525fbc118ae36747703c77
mark.paul <markpaulbusiness66@gmail.com>
2023-11-01 17:02:43 +0300

    Journey into call back function

--- script.js
+++ script.js
@@ -37,9 +37,9 @@ const checkIn = function (flightNumb, passenger) {
   passenger.name = "Mr. " + passenger.name;
 
   if (passenger.passport === 24739479284) {
-    alert("Check in");
+    console.log("Check in");
   } else {
-    alert("Wrong Passport!");
+    console.log("Wrong Passport!");
   }
 };
 
@@ -58,3 +58,57 @@ console.log(jonas.passport);
 
 // PASSING BY VALUES AND PASSING BY VALUE.
 //javascript does not pass by reference.
+
+//function that turns numbers to negative
+
+function makeNegative(number) {
+  //   console.log(number.toString());
+  const numStr = number.toString().includes("-");
+  numStr || number === 0 ? console.log(number) : console.log(-1 * number);
+}
+
+makeNegative(-5);
+makeNegative(0.12);
+makeNegative(-1);
+makeNegative(0);
+
+const arr = ["f", "g", "r", "e"];
+const mess = "Hello World";
+let mer = "";
+
+for (const x of mess) {
+  //   console.log(x + x);
+  mer += x + x;
+}
+
+console.log(mer);
+
+const oneWord = function (word) {
+  return word.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...other] = str.split(" ");
+  return [first.toUpperCase(), ...other].join(" ");
+};
+
+//HIGHER ORDER FN - TAKES IN A FUNCTION.
+const transformer = function (str, fn) {
+  console.log(str);
+  console.log(`transformed string: ${fn(str)}`);
+
+  console.log(`transformed by: ${fn.name}`);
+};
+
+transformer("ja is almighty", oneWord);
+
+transformer("javascript is the best", upperFirstWord);
+
+console.log(transformer.name);
+
+// JS uses callbacks all the tiiiiime!
+const high5 = function () {
+  console.log("WAVE");
+};
+
+["jonas", "Mark", "Adam"].forEach(high5);

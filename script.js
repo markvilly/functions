db776bc970e4da42c740710e568e6fbf3f01652b
mark.paul <markpaulbusiness66@gmail.com>
2023-11-01 11:07:58 +0300

    Functions

--- script.js
+++ script.js
@@ -26,3 +26,35 @@ createBooking("CH123", 7);
 createBooking("CH123", undefined, 1000);
 
 console.log(bookings);
+
+const flight = "LH234";
+const jonas = {
+  name: "Jonas Schmedtmann",
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNumb, passenger) {
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert("Check in");
+  } else {
+    alert("Wrong Passport!");
+  }
+};
+
+checkIn(flight, jonas);
+
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+console.log(jonas.passport);
+
+// PASSING BY VALUES AND PASSING BY VALUE.
+//javascript does not pass by reference.

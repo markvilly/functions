b0f62e17815bd3ac2e1415dda77a328711a92bed
mark.paul <markpaulbusiness66@gmail.com>
2023-11-02 09:00:02 +0300

    getting a grasp of call method.

--- script.js
+++ script.js
@@ -153,3 +153,92 @@ console.log(euroWings);
 
 book.call(lufthansa, 234, "Justus Nyabange");
 console.log(lufthansa);
+
+const swiss = {
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
+  bookings: [],
+};
+
+book.call(swiss, 538, "Mary Cooper");
+
+//Apply Method.
+const flightData = [583, "George Cooper"];
+// book.apply(swiss, flightData);
+
+book.call(swiss, ...flightData);
+console.log(swiss);
+
+// SOME EXERCISES.
+
+// Exercise 1: Function Returning Function
+// Create a function called greetMaker that takes a name as an argument and returns a function. The returned function should take another name as an argument and return a greeting string. For example, if you call greetMaker("Hello")("Alice"), it should return "Hello, Alice!"
+
+const greetMaker = function (greet) {
+  return function (name) {
+    console.log(`${greet} ${name}`);
+  };
+};
+
+greetMaker("Hello")("Mark");
+
+// Exercise 2: Sum Using Call and Apply
+// Create a function called sum that can take any number of arguments and return their sum. Use the call and apply methods to achieve this. For example, calling sum(1, 2, 3) should return 6.
+// const list = [1, 2, 3, 4];
+
+// const sum = (argumentse) => {
+//   let totalSum = 0;
+//   for (let i = 0; i < argumentse.length; i++) {
+//     totalSum += argumentse[i];
+//   }
+//   console.log(totalSum);
+// };
+
+// sum.apply(list);
+
+// Exercise 3: Calculator Object
+// Create an object called calculator with properties and methods for basic arithmetic operations (add, subtract, multiply, divide). Each method should take two arguments and return the result of the operation. For example, calling calculator.add(5, 3) should return 8.
+
+const calculator = {
+  answers: [],
+  add(a, b) {
+    this.answers.push(a + b);
+    return a + b;
+  },
+  subtract(a, b) {
+    this.answers.push(a - b);
+    return a - b;
+  },
+  multiply(a, b) {
+    this.answers.push(a * b);
+    return a * b;
+  },
+  divide(a, b) {
+    this.answers.push(a / b);
+    return a / b;
+  },
+};
+
+const add = calculator.add;
+
+const ans = {
+  answers: [],
+};
+
+calculator.divide(5, 3);
+
+calculator.add(4, 5);
+
+console.log(calculator.answers);
+
+add.call(ans, 3, 4);
+console.log(ans);
+
+// Exercise 4: Age Calculator
+// Create a function called calculateAge that returns a function. The returned function should take a birth year as an argument and return the age. Use the call method to pass the current year as this. For example, if you call calculateAge(1990)(new Date().getFullYear()), it should return the age.
+
+// Exercise 5: Function Composition
+// Create a function called compose that takes two functions as arguments and returns a new function. The new function should execute the second function with the result of the first function. For example, calling compose(square, double)(3) should return 36 because it first doubles 3 to get 6 and then squares 6 to get 36.
+
+// Exercise 1: Function Returning Function
+// Create a function called greetMaker that takes a name as an argument and returns a function. The returned function should take another name as an argument and return a greeting string. For example, if you call greetMaker("Hello")("Alice"), it should return "Hello, Alice!"

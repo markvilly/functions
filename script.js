ab46aab47e85e17fd0c6862003c022122b26dc58
mark.paul <markpaulbusiness66@gmail.com>
2023-11-05 11:14:04 +0300

    Closing Closures

--- script.js
+++ script.js
@@ -417,3 +417,84 @@ booker();
 booker();
 booker();
 booker();
+
+console.dir(booker);
+
+let f;
+
+const g = function () {
+  const a = 20;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// f reassigned can cause closure
+h();
+f();
+console.dir(f);
+
+//Example 2.
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers.`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+// const perGroup = 400;
+boardPassengers(99, 3);
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.body.addEventListener("click", () => {
+    header.style.color = "blue";
+  });
+})();
+
+// function that outerVAR();
+
+function outer() {
+  let outerVar = 23;
+  return function inner() {
+    console.log(`number is: ${outerVar}`);
+  };
+}
+
+const outerFun = outer();
+outerFun();
+
+function counter() {
+  let count = 1;
+
+  return function () {
+    console.log(`${count++} Sheep`);
+  };
+}
+
+let sheepCounter = counter();
